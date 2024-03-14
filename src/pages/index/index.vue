f4945f70f95bb9390af50ff99c3345f96c7774c4
linyicong <1256890952@qq.com>
2024-03-14 17:45:35 +0800

    change:

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,7 +6,7 @@
   <uni-card title="基础卡片" extra="额外信息">
     <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
   </uni-card>
-  <view class="index">index</view>
+  <view class="index">index2233322</view>
 </template>
 
 <style lang="scss"></style>
